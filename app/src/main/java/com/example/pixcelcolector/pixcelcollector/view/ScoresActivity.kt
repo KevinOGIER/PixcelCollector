a7b2befcb5bd16446b0b1a402c7225e7d7767321
adezouche <alexis.dezouche@hotmail.fr>
2018-04-05 12:32:29 +0200

    Changement d'activité + timer + images

--- app/src/main/java/com/example/pixcelcolector/pixcelcollector/view/ScoresActivity.kt
+++ app/src/main/java/com/example/pixcelcolector/pixcelcollector/view/ScoresActivity.kt
@@ -1,11 +1,30 @@
 package com.example.pixcelcolector.pixcelcollector.view
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import com.example.pixcelcolector.pixcelcollector.R
 import com.example.pixcelcolector.pixcelcollector.model.DataBaseHandler
 import com.example.pixcelcolector.pixcelcollector.model.Score
+import kotlinx.android.synthetic.main.activity_start.*
 
-class ScoresActivity : AppCompatActivity() {
-    
+class ScoresActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scores)
+
+        buttonPlay.setOnClickListener(this);
+    }
+
+    override fun onClick(view: View)
+    {
+q           // calling an activity using <intent-filter> action namea
+        //  Intent inent = new Intent("com.hmkcode.android.ANOTHER_ACTIVITY");
+        startActivity(intent)
+        finish()
+    }
 }
--- app/src/main/java/com/example/pixcelcolector/pixcelcollector/view/StartActivity.kt
+++ app/src/main/java/com/example/pixcelcolector/pixcelcollector/view/StartActivity.kt
@@ -7,21 +7,34 @@ import com.example.pixcelcolector.pixcelcollector.model.DataBaseHandler
 import com.example.pixcelcolector.pixcelcollector.model.Score
 import kotlinx.android.synthetic.main.activity_start.*
 import android.content.Intent
+import android.view.View
 
-class StartActivity : AppCompatActivity()
+class StartActivity : AppCompatActivity(), View.OnClickListener
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
-            val context = this
-            val db = DataBaseHandler(context)
+        val context = this
+        val db = DataBaseHandler(context)
 
-            var score1 = Score(1,"YoMama","01/01/2001")
-            db.insertData(score1)
+        var score1 = Score(1,"YoMama","01/01/2001")
+        db.insertData(score1)
 
         val temp = 0
         //textView.setText(db.getFirstScore().toString())
+
+        buttonPlay.setOnClickListener(this);
     }
+
+    override fun onClick(view: View)
+    {
+        val intent = Intent(this, GameActivity::class.java)
+        // calling an activity using <intent-filter> action name
+        //  Intent inent = new Intent("com.hmkcode.android.ANOTHER_ACTIVITY");
+        startActivity(intent)
+        finish()
+    }
+
 }
